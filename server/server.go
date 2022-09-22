78f85651dc4c556476b694692d867c192d0cf7a3
Jacob Grum <jacg@itu.dk>
2022-09-22 14:25:14 +0200

    d

--- server/server.go
+++ server/server.go
@@ -17,10 +17,10 @@ type Server struct {
 
 func (s *Server) GetHeader(ctx context.Context, message *protos.Message) (*protos.Message, error) {
 	fmt.Printf("recieved: %v", message)
-	message.Text = "Hello from server"
+	//message.Text = "Hello from server"
 	//message.Ack = 4
 	//message.Seq = 5
-	return &protos.Message{}, nil
+	return &protos.Message{Text: "Hello from server"}, nil
 }
 
 func main() {
